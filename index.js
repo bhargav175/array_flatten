c7f8ec981d73d802e296564a881a9dc28e7ce958
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2017-01-19 11:58:09 +0530

    Split reducer function from exports

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const flatten = (result,value,valueIndex,arr)=>{
+const __flattenReducer = (result,value,valueIndex,arr)=>{
   if(value instanceof Array){
-    return value.reduce(flatten,result);
+    return value.reduce(__flattenReducer,result);
   }else{
     result.push(value);
     return result;
   }
 };
+
+const flatten = function(arr){
+   if(arr instanceof Array){
+     return Array.prototype.reduce.apply(arr,[__flattenReducer,[]]);
+   }else{
+     throw new TypeError('Expected an array');
+   }
+}
 module.exports = flatten;
--- test/index.js
+++ test/index.js
@@ -8,36 +8,36 @@ test('flatten is a function',t => {
     t.is(typeof(flatten) === 'function', true);
 });
 test('flatten [] is []',t => {
-    t.deepEqual([].reduce(flatten,[]),[]);
+    t.deepEqual(flatten([]),[]);
 });
 
 test('flatten [undefined] is [undefined]',t => {
-    t.deepEqual([undefined].reduce(flatten,[]),[undefined]);
+    t.deepEqual(flatten([undefined]),[undefined]);
 });
 
 test('flatten [null,[null,[null]]] is [null,null,null]',t => {
-    t.deepEqual([null,[null,[null]]].reduce(flatten,[]),[null,null,null]);
+    t.deepEqual(flatten([null,[null,[null]]]),[null,null,null]);
 });
 
 
 test('flatten [1,2,3,4] is [1,2,3,4]',t => {
-    t.deepEqual([1,2,3,4].reduce(flatten,[]),[1,2,3,4]);
+    t.deepEqual(flatten([1,2,3,4]),[1,2,3,4]);
 });
 
 
 test('flatten [[1],2,3,4] is [1,2,3,4]',t => {
-    t.deepEqual([[1],2,3,4].reduce(flatten,[]),[1,2,3,4]);
+    t.deepEqual(flatten([[1],2,3,4]),[1,2,3,4]);
 });
 
 test('flatten [1,[2],3,4] is [1,2,3,4]',t => {
-    t.deepEqual([1,[2],3,4].reduce(flatten,[]),[1,2,3,4]);
+    t.deepEqual(flatten([1,[2],3,4]),[1,2,3,4]);
 });
 
 test('flatten [[1],[2],3,4] is [1,2,3,4]',t => {
-    t.deepEqual([[1],[2],3,4].reduce(flatten,[]),[1,2,3,4]);
+    t.deepEqual(flatten([[1],[2],3,4]),[1,2,3,4]);
 });
 
 
 test('flatten [1,[2,3,[4,5,[6,[7],8],9],10],11] is [1,2,3,4,5,6,7,8,9,10,11]',t => {
-    t.deepEqual([1,[2,3,[4,5,[6,[7],8],9],10],11].reduce(flatten,[]),[1,2,3,4,5,6,7,8,9,10,11]);
+    t.deepEqual(flatten([1,[2,3,[4,5,[6,[7],8],9],10],11]),[1,2,3,4,5,6,7,8,9,10,11]);
 });
