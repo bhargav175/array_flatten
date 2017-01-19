56dda6d52466de3d15f46802cca26d2ea4102653
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2017-01-19 10:47:51 +0530

    add travis

--- test/index.js
+++ test/index.js
@@ -7,6 +7,18 @@ test('flatten is defined',t => {
 test('flatten is a function',t => {
     t.is(typeof(flatten) === 'function', true);
 });
+test('flatten [] is []',t => {
+    t.deepEqual([].reduce(flatten,[]),[]);
+});
+
+test('flatten [undefined] is [undefined]',t => {
+    t.deepEqual([undefined].reduce(flatten,[]),[undefined]);
+});
+
+test('flatten [null,[null,[null]]] is [null,null,null]',t => {
+    t.deepEqual([null,[null,[null]]].reduce(flatten,[]),[null,null,null]);
+});
+
 
 test('flatten [1,2,3,4] is [1,2,3,4]',t => {
     t.deepEqual([1,2,3,4].reduce(flatten,[]),[1,2,3,4]);
